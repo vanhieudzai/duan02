4e02fabb1bdb3ac301144691850be488eddbe578
vanhieudzai <damhieu04.fpt@gmail.com>
2023-11-16 22:52:34 +0700

    init project

---
+++ vd1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <style>
+        body {
+            color: brown;
+        }
+    </style>
+
+</head>
+
+<body>
+    <?php
+    echo "Chào MÀY  đến với PHP 1 :)))))"
+        ?>
+</body>
+
+</html>
